573efd1e5d346602a8366ed3d5ba5c0a9089c78c
okomojacob <jaylabs2030@gmail.com>
2021-03-20 22:01:14 +0300

    Check if yr is leap or not

--- CoreyMS/isLeapYr.py
+++ CoreyMS/isLeapYr.py
@@ -1,13 +1,11 @@
 """
-This function checks, wheter or not a year passed is Leap or not
+This function checks, whether or not a year passed is Leap or not
 """
 # Number of days per month, the first is a placeholder for python indexing
 monthDays = [0, 31, 28, 31, 30, 31, 30, 31, 31,30, 31, 30, 31]
 
 def isLeap(year):
-    """
-    return True for leap year, False for non-leap years
-    """
+    """Return True for leap year, False for non-leap years"""
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0 )
 
 def daysInMonth(year, month):
