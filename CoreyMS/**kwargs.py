3eedad1eff5577e932a1e73419c7b74ea468d362
okomojacob <jaylabs2030@gmail.com>
2021-03-20 21:03:26 +0300

    new code, unpacked

--- CoreyMS/**kwargs.py
+++ CoreyMS/**kwargs.py
@@ -1,8 +1,8 @@
-def student_info(*args, **kwargs):
+def student_info(*args, **kwargs): #To accept arbitrary positional args
     print(args)
     print(kwargs)
 
 courses = ['Math', 'Calculus', 'Algebra', 'chemistry', 'Art']
 uniqueInfo = {'name': 'Jane', 'age':25}
 
-student_info(*courses, **uniqueInfo)
+student_info(*courses, **uniqueInfo) #*, ** to unpack the values
